008156b64dd2b7594f5df175486090d65ebd28f3
nandini965 <nandhini965@gmail.com>
2024-01-17 23:02:39 +0530

    filebeat

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vpc_id = length(length(vpc, main, null), vpc_id, null)
+//  vpc_id = length(length(vpc, main, null), vpc_id, null)
     tags   = {
       business_unit = "ecommerce"
       business_type = "retail"
