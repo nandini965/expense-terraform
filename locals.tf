953ee06318208d1858a2a0b06f78ae8e9c6a8573
nandini965 <nandhini965@gmail.com>
2024-01-17 21:22:37 +0530

    filebeat

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-
+  vpc_id = length(length(vpc_id, main, null), subnet_id, main, null)
     tags   = {
       business_unit = "ecommerce"
       business_type = "retail"
--- main.tf
+++ main.tf
@@ -10,3 +10,19 @@ module "vpc" {
   default_vpc_cidr = var.default_vpc_cidr
   default_vpc_rtid = var.default_vpc_rtid
 }
+
+module "app" {
+source = "git::https://github.com/nandini965/tf-module-app.expense.git"
+for_each = var.app
+subnet_name = length(length(length(vpc_id, main, null), subnet_name, main, null), subnet_ids, main, null)
+allow_app_cidr =  length(length(length(vpc_id, main, null), app_cidr, main, null), subnet_ids, main, null)
+vpc_id = length(length(vpc_id, main, null), subnet_id, main, null)
+tags = local.tags
+env = var.env
+desired_capacity = each.value["desired_capacity"]
+max_size = each.value["max_size"]
+min_size = each.value["min_size"]
+ instance_type = each.value["instance_type"]
+  subnet_name = each.value["subnet_name"]
+  app_port = each.value["app_port"]
+}
