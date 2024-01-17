8d2dcd0db70f552bd536e657790425398104f392
nandini965 <nandhini965@gmail.com>
2024-01-17 19:03:38 +0530

    filebeat

--- env-dev/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket     = "nandini24"
-key        = "expense/dev/terraform.tfstate"
-region     = "us-east-1"
--- state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket     = "nandini24"
-    key        = "expense/dev/terraform.tfstate"
-    region     = "us-east-1"
-  }
-}
