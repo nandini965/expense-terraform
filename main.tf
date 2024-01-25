bac38339f473e2fcdfed2412162d053918edb8f7
nandini965 <nandhini965@gmail.com>
2024-01-26 02:45:54 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -11,54 +11,54 @@ module "vpc" {
   default_vpc_rtid = var.default_vpc_rtid
 }
 
-module "app" {
-  depends_on = [module.vpc, module.rds, module.alb]
-  source = "git::https://github.com/nandini965/tf-module-app.expense.git"
-  name = each.value["name"]
- for_each = var.app
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
-  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnets_cidr", null)
-  listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
-  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
-  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
-  desired_capacity = each.value["desired_capacity"]
- max_size = each.value["max_size"]
- min_size = each.value["min_size"]
- instance_type = each.value["instance_type"]
-  app_port = each.value["app_port"]
-  bastion_cidr = var.bastion_cidr
-  tags = local.tags
-  env = var.env
-  kms_arn = var.kms_arn
-  listener_priority =each.value["listener_priority"]
-  domain_name = var.domain_name
-  domain_id    = var.domain_id
-
-}
-
-module "alb" {
-  source         = "git::https://github.com/nandini965/tf-module-alb.expense.git"
-  for_each       = var.alb
-  name           = each.value["name"]
-  tags           = local.tags
-  env            = var.env
-  internal       = each.value["internal"]
-  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
-  allow_alb_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnets_cidr", null)
-  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
-  vpc_id         = local.vpc_id
-}
-module "rds" {
-  source = "git::https://github.com/nandini965/tf-module-rds.expense.git"
-  for_each = var.rds
-  instance_class = each.value["instance_class"]
-  instance_count = each.value["instance_count"]
-  engine_version = each.value["engine_version"]
-  subnets     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnets_cidr", null)
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-  kms_arn = var.kms_arn
-}
+#module "app" {
+#  depends_on = [module.vpc, module.rds, module.alb]
+#  source = "git::https://github.com/nandini965/tf-module-app.expense.git"
+#  name = each.value["name"]
+# for_each = var.app
+#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnets_cidr", null)
+#  listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
+#  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
+#  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+#  desired_capacity = each.value["desired_capacity"]
+# max_size = each.value["max_size"]
+# min_size = each.value["min_size"]
+# instance_type = each.value["instance_type"]
+#  app_port = each.value["app_port"]
+#  bastion_cidr = var.bastion_cidr
+#  tags = local.tags
+#  env = var.env
+#  kms_arn = var.kms_arn
+#  listener_priority =each.value["listener_priority"]
+#  domain_name = var.domain_name
+#  domain_id    = var.domain_id
+#
+#}
+#
+#module "alb" {
+#  source         = "git::https://github.com/nandini965/tf-module-alb.expense.git"
+#  for_each       = var.alb
+#  name           = each.value["name"]
+#  tags           = local.tags
+#  env            = var.env
+#  internal       = each.value["internal"]
+#  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+#  allow_alb_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnets_cidr", null)
+#  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+#  vpc_id         = local.vpc_id
+#}
+#module "rds" {
+#  source = "git::https://github.com/nandini965/tf-module-rds.expense.git"
+#  for_each = var.rds
+#  instance_class = each.value["instance_class"]
+#  instance_count = each.value["instance_count"]
+#  engine_version = each.value["engine_version"]
+#  subnets     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnets_cidr", null)
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#  kms_arn = var.kms_arn
+#}
--- vars.tf
+++ vars.tf
@@ -3,11 +3,11 @@ variable "env" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
-variable "app" {}
+//variable "app" {}
 variable "bastion_cidr" {}
-variable "alb" {}
+//variable "alb" {}
 variable "kms_arn" {}
-variable "rds" {}
-variable "domain_id" {}
-variable "domain_name" {}
+//variable "rds" {}
+//variable "domain_id" {}
+//variable "domain_name" {}
 
